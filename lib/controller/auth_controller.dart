05022e040aad61ffb640282550e67d1c28378da5
emreozturk <emre_oztrk_99@hotmail.com>
2021-10-24 10:54:06 +0300

    Auth Controller added

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_chat_app/model/users_model.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthController extends GetxController {
+  var isSkipIntro = false.obs;
+  var isAuth = false.obs;
+
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _currentUser;
+  UserCredential? userCredential;
+
+  var user = UsersModel().obs;
+
+  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+
+  Future<void> firstInitialized() async {
+    await autoLogin().then((value) {
+      if (value) {
+        isAuth.value = true;
+      }
+    });
+    await skipIntro().then((value) {
+      if (value) {
+        isSkipIntro.value = true;
+      }
+    });
+  }
+
+  Future<bool> skipIntro() async {}
+
+  Future<bool> autoLogin() async {}
+
+  Future<void> login() async {}
+
+  Future<void> logout() async {}
+
+  void changeProfile(String name, String status) {}
+
+  void updateStatus(String status) {}
+
+  void updatePhotoUrl(String url) async {}
+
+  void addNewConnection(String friendEmail) async {}
+}
